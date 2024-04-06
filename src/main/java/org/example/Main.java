4692c52997fe65e405d479d8475fdc06e1f22f0c
DmytroKaplun <newtonknight1990@gmail.com>
2024-04-06 13:38:24 +0300

    fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import com.google.gson.Gson;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyProperties name = MyProperties.getInstance();
+        System.out.println(new Gson().toJson(name));
+        System.out.println("hello world");
     }
 }
--- src/main/java/org/example/MyProperties.java
+++ src/main/java/org/example/MyProperties.java
@@ -1,2 +1,19 @@
-package org.example;public class MyProperties {
+package org.example;
+
+public class MyProperties {
+    private final String name;
+    private final String lastName;
+
+    private MyProperties() {
+        name = "Dmytro";
+        lastName = "Kaplun";
+    }
+
+    private static final class InstanceHolder {
+        private static final MyProperties instance = new MyProperties();
+    }
+
+    public static MyProperties getInstance() {
+        return InstanceHolder.instance;
+    }
 }
